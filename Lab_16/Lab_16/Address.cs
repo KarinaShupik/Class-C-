5821d2101e5b4e1191d1d3c9c0106febce1b9ff0
Karina <105536477+KarinaShupik@users.noreply.github.com>
2023-03-10 15:40:20 +0200

    Class C#

---
+++ Lab_16/Lab_16/Address.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace Lab_16
+{
+    class Address
+    {
+        /*
+         Побудувати опис класу, що містить інформацію про поштову адресу
+        організації. Передбачити можливість роздільної зміни складових частин
+        адреси, створення й знищення об’єктів цього класу.*/
+
+        /*Розробити клас, який повинен містити три конструктори, властивості, по
+        можливості передбачити статичні елементи класу та перевантаження методів.
+        Розробити Windows Form додаток, яка демонструє роботу класу. */
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Flat { get; set; }
+        public string Email { get; set; }
+        public string LocationName { get; set; }
+        public string LocationRegion { get; set; }
+        public string LocationArea { get; set; }
+        public string CountryName { get; set; }
+        public string Index { get; set; }
+
+        //без параметрів
+        //конструктори
+        //ім'я користувача
+        public Address(string name)
+        {
+            Name = name;
+        }
+        //телефон та email користувача
+        public Address(string number, string email)
+        {
+            Number = number;
+            Email = email;
+        }
+        //локація проживання користувача 
+        public Address(string street, string house, string flat, string location, string region, string area)
+        {
+            Street = street;
+            House = house;
+            Flat = flat;
+            LocationName = location;
+            LocationRegion = region;
+            LocationArea = area;
+        }
+
+        public Address(string user, string phone, string street, string house, string flat, string email, string locationName, string region, string area, string country, string index)
+        {
+            Name = user;
+            Number = phone;
+            Street = street;
+            House = house;
+            Flat = flat;
+            Email = email;
+            LocationName = locationName;
+            LocationRegion = region;
+            LocationArea = area;
+            CountryName = country;
+            Index = index;
+        }
+
+        public override string ToString()
+        {
+            return $"Найменування адресата: {Name}\n" +
+                   $"Номер мобільного: {Number}\n" +
+                   $"Найменування вулиці: {Street}\n" +
+                   $"Номер будинку: {House}\n" +
+                   $"Номер квартири: {Flat}\n" +
+                   $"Електронна почта: {Email}\n" +
+                   $"Найменування населеного пункту: {LocationName}\n" +
+                   $"Найменування району: {LocationRegion}\n" +
+                   $"Найменування області: {LocationArea}\n" +
+                   $"Країна призначення: {CountryName}\n" +
+                   $"Індекс: {Index}";     
+        }
+
+        public void Print()
+        {
+            MessageBox.Show(ToString());
+        }
+
+    }
+}
---
+++ Lab_16/Lab_16/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab_16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_res_Click(object sender, RoutedEventArgs e)
+        {
+            string user_name = txt1.Text;//найменування адресата
+            string phone_num = txt2.Text; //номер мобільного
+            string street_name = txt3.Text;//найменування вулиці
+            string house_num = txt4.Text;//номер будинку
+            string flat_num = txt5.Text;//номер квартири
+            string email = txt6.Text;//почта
+            string location_name = txt7.Text;//найменування населеного пункту
+            string region_name = txt8.Text;//найменування району
+            string area_name = txt9.Text;//найменування області
+            string country_name = txt10.Text;//країна призначення
+            string index = txt11.Text;//країна призначення
+
+           
+            //для перевірки кількості символів у текстовому полі індексу
+            if (index.Length == 5 && int.TryParse(index, out int number1))
+            {
+                index = txt11.Text;
+            }
+            else
+            {
+                index = "Error";
+            }
+
+            //для перевірки кількості символів у текстовому полі мобільного телефону
+            if (phone_num.Length == 10 && int.TryParse(phone_num, out int number2))
+            {
+                phone_num = txt2.Text;
+            }
+            else
+            {
+                phone_num = "Error";
+            }
+
+            Address user = new Address(user_name, phone_num, street_name, house_num, flat_num, email, location_name, region_name, area_name, country_name, index);
+            user.Print();
+
+        }
+    }
+}
